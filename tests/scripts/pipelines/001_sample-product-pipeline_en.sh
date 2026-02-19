94cf5f76e4ec6c07020e05222b681350644bbfba
karsten-klein <karsten.klein@metaeffekt.com>
2026-02-19 13:08:12 +0100

    AE-1698: PR review
    - adjsuting parameters for reference inventory

--- tests/scripts/pipelines/001_sample-product-pipeline_en.sh
+++ tests/scripts/pipelines/001_sample-product-pipeline_en.sh
@@ -32,7 +32,7 @@ set_global_variables() {
   logger_init "$LOG_DIR/001_sample-product-pipeline_en.log"
   create_workspace_directories "$WORKSPACE_DIR/sample-product-1.0.0" "sample-asset-1.0.0"
 
-  ENV_REFERENCE_INVENTORY_DIR="$WORKBENCH_DIR/inventories/example-reference-inventory"
+  ENV_REFERENCE_INVENTORY_DIR="$WORKBENCH_DIR/inventories/example-reference-inventory/inventory"
   ENV_REFERENCE_LICENSES_DIR="$WORKBENCH_DIR/inventories/example-reference-inventory/licenses"
   ENV_REFERENCE_COMPONENTS_DIR="$WORKBENCH_DIR/inventories/example-reference-inventory/components"
 
